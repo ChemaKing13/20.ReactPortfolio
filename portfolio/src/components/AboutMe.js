7363ed2f75ff3af69aa8f85c8316ac634d4b72f8
ChemaKing13 <jm2000ho@gmail.com>
2023-07-31 00:22:59 -0600

    image from aboutMe.js added

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
+
 const AboutMe = () => {
   return (
-    <div className="about-me">
-      <h2>About Me</h2>
-      <p>
-        {/* Your introduction or description */}
-        Hi! My name is [Your Name], and I am a passionate web developer. I love building
-        creative and user-friendly web applications using the latest technologies.
-      </p>
-      <p>
-        {/* More about yourself */}
-        In my free time, I enjoy learning new programming languages and frameworks. I am
-        also a huge fan of open-source projects and actively contribute to the developer
-        community.
-      </p>
-      {/* You can add more sections with additional information */}
-    </div>
+    <section className="about-me">
+      <div className="container">
+        <div className="about-me-content">
+          <div className="about-me-text">
+            <h2>About Me</h2>
+            <p>
+              Welcome to my portfolio website! My name is [Your Name], and I am a passionate web developer based in [Your Location]. I enjoy creating interactive and user-friendly web applications using the latest technologies.
+            </p>
+            <p>
+              [More about yourself and your experiences, skills, and interests.]
+            </p>
+          </div>
+          <div className="about-me-image">
+            <img src='./images/pic.JPG' alt="My Picture" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
 export default AboutMe;
+
