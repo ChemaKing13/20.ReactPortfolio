be72074bbf3c1217d056884fe0437e74b7e10afd
ChemaKing13 <jm2000ho@gmail.com>
2023-07-31 11:29:17 -0600

    aboutMe section updated

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const name = 'José María Hernández'
+const age = '23'
 
 const AboutMe = () => {
   return (
@@ -7,7 +9,12 @@ const AboutMe = () => {
       <div className="container">
         <div className="about-me-content">
           <div className="about-me-text">
-            <h2>About Me</h2>
+            <p>Hi, my name is {name}, I'm {age} years old, im an engineering studend. I'm really passionate about music and sports.<br/>
+               Right now I'm incredibly excited cause I am on the week 20 of my bootcamp for Fullstack Web Development. It has been a journey, <br/>
+               and although it has been challengeing and sometimes stressfull but the effort is worth it. I have notice that i really <br/>
+               enjoy coding. I know it's just the begining of my career and learning will never end, but I'm really sure that theres nothing that discipline and passion can't do.
+               This is my Portfolio where i would like to showcase some of my projects and also provide my contact information, hope you enjoy!!
+            </p>
           </div>
           <div className="about-me-image animation-bounce">
             <img src='./images/pic.JPG' alt="My Picture" />
