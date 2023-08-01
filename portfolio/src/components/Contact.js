29f2c0a8a53974f4aadb68d4cde946813176f33e
ChemaKing13 <jm2000ho@gmail.com>
2023-08-01 00:20:53 -0600

    helper to verify email set

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  };
+
   return (
-    <div className="contact">
-      <h2>Contact</h2>
-    </div>
+    <section className="contact">
+      <div className="container">
+        <div className="contact-form">
+          <h2>Get in Touch</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea id="message" name="message" rows="5" required></textarea>
+            </div>
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 };
 
