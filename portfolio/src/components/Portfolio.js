4aec9912692ee4cd92b596980253516b06852b25
ChemaKing13 <jm2000ho@gmail.com>
2023-08-02 01:40:54 -0600

    portfolio.js ready

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const projects = [
   {
@@ -6,36 +8,42 @@ const projects = [
     title: 'Opti Crew',
     imageSrc: './images/opti.png',
     description: 'Description of Project 1',
+    githubLink: 'https://github.com/jony0418/OptiCrew'
   },
   {
     id: 2,
-    title: 'Progressive Web App',
+    title: 'Password Strength Tester',
     imageSrc: './images/password.png',
     description: 'Description of Project 2',
+    githubLink: 'https://github.com/ChemaKing13/PasswordStrengthTester'
   },
   {
     id: 3,
     title: 'Meal PreApp',
     imageSrc: './images/meal.png',
     description: 'Description of Project 2',
+    githubLink: 'https://github.com/Maelikah/P1_MealPreApp'
   },
   {
     id: 4,
-    title: 'Tech Blog',
+    title: 'Note Taker',
     imageSrc: './images/note.png',
     description: 'Description of Project 2',
+    githubLink: 'https://github.com/ChemaKing13/11.NoteTaker'
   },
   {
     id: 5,
     title: 'Social Network Api',
     imageSrc: './images/ecommerce.png',
     description: 'Description of Project 2',
+    githubLink: 'https://github.com/ChemaKing13/13.E-CommerceBackEnd'
   },
   {
     id: 6,
     title: 'Project 6',
     imageSrc: './images/underconst.png',
     description: 'Description of Project 2',
+    githubLink: '',
   },
 ];
 
@@ -49,8 +57,14 @@ const Portfolio = () => {
           {projects.map((project) => (
             <div key={project.id} className="project">
               <img src={project.imageSrc} alt={project.title} />
-              {/* <h3>{project.title}</h3>
-              <p>{project.description}</p> */}
+              <div className='content'>
+                <h1>{project.title}</h1>
+                {project.githubLink && (
+                  <a href={project.githubLink} target='_blank' rel='noopener noreferrer'>
+                  <FontAwesomeIcon className='github-icon' icon={faGithub} />
+                  </a>
+                )}
+              </div>
             </div>
           ))}
         </div>
